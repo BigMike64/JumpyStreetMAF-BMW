f24784190705cd4d66208cf89af3c2c41390033c
BigMike64 <kingmikeike2016@gmail.com>
2021-09-15 15:02:37 -0400

    Chicken Movement

--- Jumpy-Street-Project/Assets/Scripts/PlayerMovement.cs
+++ Jumpy-Street-Project/Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
-    float playerSpeed = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +13,21 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            transform.position = transform.position + new Vector3(0, 2.5f);
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            transform.position = transform.position + new Vector3(0, -2.5f);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            transform.position = transform.position + new Vector3(2.5f, 0);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            transform.position = transform.position + new Vector3(-2.5f, 0);
+        }
     }
 }
